19c4688823bf3b466c1cc16a29a24ec51f583078
InventorBoy-APP <78768396+InventorBoy-APP@users.noreply.github.com>
2021-10-11 16:34:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload(){
+    ""
+}
+let c = " "
+
+function setup(){
+    canvas= createCanvas( 500, 400 );
+    canvas.position( 100, 400);
+    video= createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,90,80,300,300);
+    circle(80,30,50);
+    circle(400,30,50)
+    circle(80,400,50)
+    circle(400,400,50)
+    let c = color('#32CD32')
+    fill(c);
+    noStroke();
+    
+line(30, 20, 85, 75);
+
+}
+
+function take_snapshot(){
+    save('welcome.png');
+}
